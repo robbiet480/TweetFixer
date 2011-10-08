3ef2bf2e75785e5262732c3f061de50355e73a46
Robbie Trencheny <me@robbiet.us>
2011-10-08 12:54:39 -0700

    0.0.2, much cleaner code. Added YOURLS support, colors, and a few sanity checks.

--- README.markdown
+++ README.markdown
@@ -1,26 +1,58 @@
 # TweetFixer
 ## Description
-TweetFixer will keep an eye on your Twitter account and when it finds a URL, it will replace it with a custom short url from your bit.ly account. It will also drill down any short links it finds until it reaches the root URL
+TweetFixer will keep an eye on your Twitter account and when it sees a URL, it will delete the tweet and replace it with a custom short url from your bit.ly account or a [YOURLS](http://yourls.org/) shortener (along with the existing tweet text). It will also drill down any short links it finds until it reaches the root URL, courtesy of [longurl.org](http://longurl.org). All of this takes place within 1 second, so most people won't ever notice a thing.
 
 ## Purpose
-I got tired of not having better analytics on the links I was sending out and, in some cases, generating money for other people because they used a shortener which had ads. Bit.ly's free account provides pretty good analytics on all links.
+I got tired of not having better analytics on the links I was sending out on Twitter and, in some cases, generating money for other people because they used a shortener which had ads. Bit.ly's free account and YOURLS both provide pretty good analytics on links.
 
 ## Requirements
-* [ntwitter](https://github.com/AvianFlu/ntwitter) MAKE SURE YOU GET ntwitter FROM THE GITHUB REPO. The one in NPM is broken right now, even though they have the same version
+* [ntwitter](https://github.com/AvianFlu/ntwitter)
 * [node-bitly](https://github.com/tanepiper/node-bitly)
-
+* [colors.js](https://github.com/marak/colors.js)
 
 ## Setup
-* Configure your Bit.ly username, and API on line 6. 
-* Configure your consumer_key, consumer_secret, access\_token\_key and access\_token\_secret on lines 9-12. 
-* On line 79, set your own user id.
-* On line 88 change YOUR DOMAIN and YOUR APP NAME to whatever you used to sign up. Make sure it is exact. You can also get this by viewing the JSON for any tweet and copy/pasting the source child in here.
+* Make a new file named config.json in the root directory.
+* Put this in there:
+
+```json
+    settings = {
+        "useragent": "tweetfixer/0.0.2 by Robbie Trencheny <http://github.com/robbiet480/tweetfixer>",
+        "shortener": "yourls",
+        "twitter": {
+            "consumer_key": "",
+            "consumer_secret": "",
+            "access\_token_key": "",
+            "access\_token_secret": "",
+            "user_id": "46713",
+            "source": "<a href=\"Domain.com\" rel=\"nofollow\">App Name</a>"
+        },
+        "bitly": {
+            "username": "",
+            "api\_key": ""
+        },
+        "yourls": {
+            "username":"",
+            "password":"",
+            "domain":""
+        }
+    }
+```
+* Change the values to suit your needs. For shortener, the valid values are yourls or bitly
+* Save config.json in the root directory
+* Run `node app.js`
 
 ## Notes
-You need to setup a new application on your Twitter account. To do this, go to [dev.twitter.com](http://dev.twitter.com), sign in and [create a new app](https://dev.twitter.com/apps/new). Name it whatever you want (preferably something without apostrophes etc). Make sure you change the permissions model to read AND write. Then go back to the main app page and at the bottom click generate access details to get the access\_token\_key and access\_token\_secret.
+You need to setup a new application on your Twitter account. To do this, go to [dev.twitter.com](http://dev.twitter.com), sign in and [create a new app](https://dev.twitter.com/apps/new). Name it whatever you want (preferably something without apostrophes etc). Make sure you change the permissions model to read AND write. Then go back to the main app page and at the bottom click generate access details to get the access\_token\_key and access\_token\_secret. All the keys and secrets go into the config.json file
+
+## Acknowledgements
+* Ankur Oberoi for the function/callback help
 
 ## Running
 `node app.js`
+
+## Version History
+0.0.2: Separate settings file, YOURLS support.
+0.0.1: Initial release. bit.ly support only
 
 ## Contributing
 Fork it, change it, pull request it
--- app.js
+++ app.js
@@ -1,23 +1,81 @@
+//TweetFixer 0.0.2
+//requires
 var twitter = require('ntwitter')
-    ,Bitly = require('Bitly').Bitly
     ,http = require('http')
+    ,color = require('colors')
+    ,Bitly = require('bitly').Bitly
+    ,fs = require('fs')
     ,sys = require('sys')
+//Read in config file
+var settings = eval(fs.readFileSync('config.json', encoding="ascii"));
 
-var bitly = new Bitly('USERNAME', 'API KEY');
-
+//Setup Twitter
 var twit = new twitter({
-    consumer_key: 'CONSUMER KEY',
-    consumer_secret: 'CONSUMER SECRET',
-    access_token_key: 'ACCESS TOKEN KEY',
-    access_token_secret: 'ACCESS TOKEN SECRET'
+    consumer_key: settings.twitter.consumer_key,
+    consumer_secret: settings.twitter.consumer_secret,
+    access_token_key: settings.twitter.access_token_key,
+    access_token_secret: settings.twitter.access_token_secret
 });
 
+//Check if Bitly is enabled, if so, set it up!
+if(settings.shortener == "bitly") {
+var bitly = new Bitly(settings.bitly.username, settings.bitly.api_key);
+}
+
+
+//Set some variables
 var new_url;
 var new_json;
 var data;
 var chunk;
 var final_url = '';
 
+//Ok! Script loaded
+console.log(color.green(color.underline('TweetFixer 0.0.2, started')));
+
+//determine which URL shortener to use
+function pick_shortener(url1, cb) {
+    if(settings.shortener == "yourls") {
+        yourls_shorten(url1, function(result) {
+           cb(result); 
+        });
+    } else if(settings.shortener == "bitly") {
+        bitly.shorten(url1, function(result) {
+            //console.log(result.data.url);
+            cb(result.data.url);
+        });
+
+
+    }
+}
+
+//YOURLS shortener function
+function yourls_shorten(url1, cb) {
+    request = http.request({
+        host: settings.yourls.domain,
+        port: 80,
+        path: '/yourls-api.php?username='+settings.yourls.username+'&password='+settings.yourls.password+'&action=shorturl&format=simple&url=' + escape(url1),
+        method: 'GET',
+        headers: {
+            'User-Agent': settings.useragent
+        }
+    },
+    function(response) {
+        response.setEncoding('utf8');
+        var responseData = '';
+        response.on('data',
+        function(chunk) {
+            responseData += chunk;
+        });
+        response.on('end',
+        function() {
+            cb(responseData);
+        });
+    });
+    request.end();
+}
+
+//Unshorten via longurl.org function
 function unshorten(url1, cb) {
     request = http.request({
         host: 'api.longurl.org',
@@ -25,7 +83,7 @@ function unshorten(url1, cb) {
         path: "/v2/expand?format=json&url=" + escape(url1),
         method: 'GET',
         headers: {
-            'User-Agent': 'tweetfixer/1.0'
+            'User-Agent': settings.useragent
         }
     },
     function(response) {
@@ -46,20 +104,23 @@ function unshorten(url1, cb) {
     request.end();
 }
 
+//The brains of the script. Checks incoming tweets for URL's, if it exists, delete and tweet with our own URL
 function FixURL(str, id) {
-    var url_match = new RegExp(/https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w/_\.]*(\?\S+)?)?)?/);
+    var url_match = new RegExp(/https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w/_\.]*(\?\S+)?)?)?/); // fragment locater
     if (!url_match.exec(str)) {
+        console.log(color.red(color.underline('Skipping this:')) + ' '+str);
         return false;
     } else {
-        / / check string
     var string = url_match.exec(str);
     unshorten(string[0],
     function(url) {
         var final_url = url;
-        console.log(final_url);
-        bitly.shorten(final_url,
+        pick_shortener(final_url,
         function(result) {
-            var new_tweet = string['input'].replace(string[0], result.data.url);
+            var new_tweet = string['input'].replace(string[0], result);
+            console.log(color.yellow(color.underline('Original tweet:')) + ' '+str);
+            console.log(color.red(color.underline('Deleting this:')) + ' '+id);
+            console.log(color.blue(color.underline('Tweeting this:')) + ' '+new_tweet);
             twit.destroyStatus(id,
             function(err, data) {
                 //console.log('destroyed: ' + console.dir(data))
@@ -75,22 +136,27 @@ function FixURL(str, id) {
 
 }
 }
+//Start the Twitter stream
 twit.stream('statuses/filter', {
-    'follow': 'TWITTER USER ID'
+    'follow': settings.twitter.user_id
 },
 function(stream) {
     stream.on('data',
     function(data) {
+        //Uncomment below to show full data from Twitter stream
         //console.log(data);
+        if(data.delete) {
+            console.log(color.red(color.underline('Tweet deleted:')) + ' '+data.delete.status.id);
+        } else {
         var text = data.text;
         var id = data.id_str;
         var source = data.source;
-        if (source !== '<a href="YOUR DOMAIN" rel="nofollow">YOUR APP NAME</a>') {
+        if (source !== settings.twitter.source) {
             var url = FixURL(text, id);
         } else {
-            console.log('Not tweeting, since I would cause a loop!');
+            console.log(color.red(color.underline('Not tweeting, since I would cause a loop!')));
+            }
         }
 
     });
 });
-
